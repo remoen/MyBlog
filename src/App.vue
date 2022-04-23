d1794ed9fd8ef615bba04fde82e83ce6d9634038
remoen <idongho152@gmail.com>
2022-04-24 00:02:06 +0900

    add page changing, some motions

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
           <a id="write" v-on:click="write()">글 쓰기</a>
         </li>
         <li>
-          <a id="listA" v-on:click="list()">글 목록</a>
+          <a id="listA" v-on:click="list(1)">글 목록</a>
         </li>
       </ul>
     </nav>
@@ -41,11 +41,16 @@
       <ul>
         <li v-for="data in pageData" v-bind:key="data">
           <div id="listBoxes">
-            <a :href="data.id">&nbsp;&nbsp;id: {{ data.id }} | {{ data.title }}</a>
+            <a @click="post(data.id)">&nbsp;&nbsp;id: {{ data.id }} | {{ data.title }}</a>
           </div>
           <div id="between"></div>
         </li>
       </ul>
+      <div id="pageSelector">
+        <div v-for="page in defMaxPage()" v-bind:key="page" style="display: flex; padding: 0.5em; float: left">
+          <button @click="list(page)">{{ page }}</button>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -53,11 +58,28 @@
     <h2>글 쓰기</h2>
     <hr class="listLine">
     <div id="inputs">
-      <div id="inputTitle" class="inputsTexts">제목 <input v-model="title" maxlength="100" class="beforeWrite"
+      <div id="inputTitle" class="inputsTexts">제목 <input v-model="title" maxlength="100"
+                                                         class="beforeWrite"
                                                          placeholder="여기에 제목을 입력해주세요"></div>
-      <div id="inputDetail" class="inputsTexts"><h4>내용</h4> <textarea v-model="texts" maxlength="3000" class="mainInput"
-                                                                      placeholder="여기에 내용을 작성해주세요"></textarea></div>
+      <div id="inputDetail" class="inputsTexts"><h4>내용</h4> <textarea v-model="texts"
+                                                                      maxlength="3000"
+                                                                      class="mainInput"
+                                                                      placeholder="여기에 내용을 작성해주세요"></textarea>
+      </div>
       <button v-on:click="submit(title, texts)">게시하기</button>
+    </div>
+  </div>
+
+  <div class="page" id="post" v-if="page==='post'">
+    <div id="showTitle">
+      <div id="back">
+        <button @click="back()">뒤로가기</button>
+      </div>
+      <h1>{{ postData.title }}</h1>
+    </div>
+    <hr class="listLine">
+    <div>
+      <p v-html="postDataTexts"></p>
     </div>
   </div>
   </body>
@@ -76,21 +98,36 @@ export default {
       title: '',
       texts: '',
       pageList: '1',
-      pageData: ''
+      pageData: '',
+      postData: '',
+      postDataTexts: '',
+      readingPage: '1',
+      maxPage: '1'
     }
   },
   methods: {
+    post(id) {
+      this.page = 'post'
+
+      axios({
+        url: 'http://222.100.125.234:3000/post/' + id
+      }).then(response => this.process(response))
+    },
+    process(response) {
+      this.postData = response.data
+      this.postDataTexts = response.data.texts.toString().replace(/\n/gim, '<br>')
+    },
     read() {
       this.page = 'read'
     },
     write() {
       this.page = 'write'
     },
-    list() {
+    list(page) {
       this.page = 'main'
 
       axios({
-        url: 'http://222.100.125.234:3000/page/1',
+        url: 'http://222.100.125.234:3000/page/'+page,
 
       }).then(response => this.pageData = response.data)
     },
@@ -110,18 +147,28 @@ export default {
             "Content-Type": `application/json`,
           },
         })
-            .then((res) => {
-              alert(res.data);
-              this.page = 'read'
-            })
-            .catch(function (error) {
-              alert(error + " 오류가 발생했습니다 다시 시도해주세요 \n반복된다면 관리자에게 연락 바랍니다")
-            })
+          .then((res) => {
+            alert(res.data);
+            this.page = 'read'
+          })
+          .catch(function (error) {
+            alert(error + " 오류가 발생했습니다 다시 시도해주세요 \n반복된다면 관리자에게 연락 바랍니다")
+          })
       } else {
         alert("4자리의 숫자만 가능합니다")
       }
-    }
-  },
+    },
+    back() {
+      this.page = 'main'
+    },
+    defMaxPage() {
+      axios({
+        url: 'http://222.100.125.234:3000/maxPage'
+      }).then(response => this.maxPage = response.data)
+
+      return this.maxPage
+    },
+  }
 }
 
 </script>
