73aa8ae59ec575d5530ae4af96cb4d435b40290d
remoen <idongho152@gmail.com>
2022-04-23 21:26:32 +0900

    now it views id, title at lists

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,67 @@
 <template>
+  <html lang="ko">
   <body>
-    <header id="header">
-      <h1 id="title">
-        <a v-on:click="read()">MyBlog</a>
-      </h1>
-      <nav class="links">
-        <ul>
-          <li>
-            <a id="write" v-on:click="write()">글 쓰기</a>
-          </li>
-          <li>
-            <a id="listA" v-on:click="list()">글 목록</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-
-    <div class="page" id="read" v-if="page==='read'">
-      <h2>반가워요!</h2>
-      <hr class="listLine">
-      <h3>MyBlog</h3>
+  <header id="header">
+    <h1 id="title">
+      <a v-on:click="read()">MyBlog</a>
+    </h1>
+    <nav class="links">
       <ul>
         <li>
-          개발자 공부용
+          <a id="write" v-on:click="write()">글 쓰기</a>
         </li>
         <li>
-          로그인 없이 글, 댓글 작성
+          <a id="listA" v-on:click="list()">글 목록</a>
         </li>
-        <li>
-          오픈 소스 (<a href="https://github.com/remoen">클릭</a>)
+      </ul>
+    </nav>
+  </header>
+
+  <div class="page" id="read" v-if="page==='read'">
+    <h2>반가워요!</h2>
+    <hr class="listLine">
+    <h3>MyBlog</h3>
+    <ul>
+      <li>
+        개발자 공부용
+      </li>
+      <li>
+        로그인 없이 글, 댓글 작성
+      </li>
+      <li>
+        오픈 소스 (<a href="https://github.com/remoen">클릭</a>)
+      </li>
+    </ul>
+  </div>
+
+  <div class="page" id="list" v-if="page==='main'">
+    <h2>글 목록</h2>
+    <hr class="listLine">
+    <div id="listPage">
+      <ul>
+        <li v-for="data in pageData" v-bind:key="data">
+          <div id="listBoxes">
+            <a :href="data.id">&nbsp;&nbsp;id: {{ data.id }} | {{ data.title }}</a>
+          </div>
+          <div id="between"></div>
         </li>
       </ul>
     </div>
+  </div>
 
-    <div class="page" id="list" v-if="page==='main'">
-      <h2>글 목록</h2>
-      <hr class="listLine">
-      <div id="listPage">
-        {{ pageData }}
-      </div>
+  <div class="page" id="writeDiv" v-if="page==='write'">
+    <h2>글 쓰기</h2>
+    <hr class="listLine">
+    <div id="inputs">
+      <div id="inputTitle" class="inputsTexts">제목 <input v-model="title" maxlength="100" class="beforeWrite"
+                                                         placeholder="여기에 제목을 입력해주세요"></div>
+      <div id="inputDetail" class="inputsTexts"><h4>내용</h4> <textarea v-model="texts" maxlength="3000" class="mainInput"
+                                                                      placeholder="여기에 내용을 작성해주세요"></textarea></div>
+      <button v-on:click="submit(title, texts)">게시하기</button>
     </div>
-
-    <div class="page" id="writeDiv" v-if="page==='write'">
-      <h2>글 쓰기</h2>
-      <hr class="listLine">
-      <div id="inputs">
-        <div id="inputTitle" class="inputsTexts">제목 <input v-model="title" maxlength="100" class="beforeWrite"
-                                                           placeholder="여기에 제목을 입력해주세요"></div>
-        <div id="inputDetail" class="inputsTexts"><h4>내용</h4> <textarea v-model="texts" maxlength="3000" class="mainInput"
-                                                                        placeholder="여기에 내용을 작성해주세요"></textarea></div>
-        <button v-on:click="submit(title, texts)">게시하기</button>
-      </div>
-    </div>
+  </div>
   </body>
+  </html>
 </template>
 
 <script>
@@ -81,7 +90,8 @@ export default {
       this.page = 'main'
 
       axios({
-        url: 'http://222.100.125.234:3000/page',
+        url: 'http://222.100.125.234:3000/page/1',
+
       }).then(response => this.pageData = response.data)
     },
     submit(title, texts) {
